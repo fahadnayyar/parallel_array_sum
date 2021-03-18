7dfc6ea9e24e787f9a68c09a8a9e9cc7ab1edcdf
Fahad <fahad.nayyar10@gmail.com>
2021-03-18 14:16:43 +0530

    added testing scripts

--- Makefile
+++ Makefile
@@ -1,14 +1,25 @@
-default: test 
-
-test: parallel_array_sum.o test.cpp
-	g++ -g test.cpp parallel_array_sum.o -o test -lpthread
+default: speedup_test random_tests interactive
 
 parallel_array_sum.o: parallel_array_sum.cpp parallel_array_sum.h
 	g++ -g parallel_array_sum.cpp -c -o parallel_array_sum.o -lpthread
 
-run:
-	./test
+random_tests: parallel_array_sum.o random_tests.cpp
+	g++ -g random_tests.cpp parallel_array_sum.o -o random_tests -lpthread
+
+interactive: parallel_array_sum.o interactive.cpp
+	g++ -g interactive.cpp parallel_array_sum.o -o interactive -lpthread
+
+speedup_test: parallel_array_sum.o speedup_test.cpp
+	g++ -g speedup_test.cpp parallel_array_sum.o -o speedup_test -lpthread
+
+run_random_tests:
+	./random_tests
+
+run_speedup_test:
+	./speedup_test
 
 clean:
-	rm ./test
+	rm random_tests
+	rm speedup_test
+	rm interactive
 	rm *.o
---
+++ speedup_test.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <sys/sysinfo.h>
+#include "parallel_array_sum.h"
+
+#define DEFAULT_ARRAY_SIZE 1024*1024*128
+
+ll * generate_array (ll input_array_size) {
+  ll * input_array = (ll *) malloc(sizeof(ll)*input_array_size); 
+  for (int i=0; i<input_array_size; i++) {
+     input_array[i] = i%100000;
+  }
+  return input_array;
+}
+
+bool verify ( ll parallel_sum_total, ll* input_array, ll input_array_size ) {
+   
+   ll actual_sum = 0;
+   for (int i=0; i<input_array_size; i++) {
+      actual_sum += input_array[i];
+   }
+   # ifdef DEBUG
+      printf("actual_sum: %lld\n", actual_sum);
+      printf("parallel_sum_total: %lld\n", parallel_sum_total);
+   #endif
+   if (parallel_sum_total == actual_sum) {
+      // # ifdef DEBUG
+         printf("Sum of array is correct.\n");
+      // #endif
+      return true;
+   }else {
+      // # ifdef DEBUG   
+         printf("Sum of array is incorrect.\n");
+      // #endif
+      return false;
+   }
+
+}
+
+
+void print_parallel_sum_time (ll input_array_size, ll * input_array, int number_of_threads) {
+
+   struct timespec begin, end; 
+   clock_gettime(CLOCK_REALTIME, &begin);
+
+   parallel_sum_namespace::ParallelSum parallel_sum (input_array, input_array_size, number_of_threads);
+   ll total_parallel_sum = parallel_sum.giveParallelSumOfArray();
+
+   clock_gettime(CLOCK_REALTIME, &end);
+   long seconds = end.tv_sec - begin.tv_sec;
+   long nanoseconds = end.tv_nsec - begin.tv_nsec;
+   double elapsed = seconds + nanoseconds*1e-9;
+   std::cout  << "Total time taken (millisec): " << elapsed*1000 << std::endl;
+
+   verify(total_parallel_sum, input_array, input_array_size);
+   
+}
+
+int main ( int argc, char *argv[] ) {
+   
+   char * input_array_size_ptr = argv[1];
+   ll input_array_size;
+   if (input_array_size_ptr == NULL) {
+      input_array_size = DEFAULT_ARRAY_SIZE;
+   }else {
+      input_array_size = atoi(input_array_size_ptr);
+   }
+ 
+   ll * input_array = generate_array(input_array_size);
+
+   print_parallel_sum_time (input_array_size, input_array, 1);
+   print_parallel_sum_time (input_array_size, input_array, 2);
+   print_parallel_sum_time (input_array_size, input_array, 4);
+   print_parallel_sum_time (input_array_size, input_array, 8);
+   print_parallel_sum_time (input_array_size, input_array, 16);   
+   
+   free(input_array);
+
+   return 0;
+   
+}
